ecf5e3bf832392f61f3994293ea953d7e1c60045
키위 <slalom0914@gmail.com>
2025-12-17 18:29:11 +0900

    서블릿과jsp페이지 연결해 보기

--- src/main/java/lab/ch01/CrudDeptServlet.java
+++ src/main/java/lab/ch01/CrudDeptServlet.java
@@ -1,9 +1,14 @@
 package lab.ch01;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -33,6 +38,34 @@ public class CrudDeptServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		log.info("doGet");
+		//insert here - 조회결과 쥐고 있다.
+		List<Map<String,Object>> list = new ArrayList<>();
+		Map<String, Object> map = new HashMap<>();
+		map.put("deptno",10);
+		map.put("dname","총무부");
+		map.put("loc","서울");
+		list.add(map);
+		map = new HashMap<>();
+		map.put("deptno",20);
+		map.put("dname","개발부");
+		map.put("loc","부산");
+		list.add(map);
+		map = new HashMap<>();
+		map.put("deptno",30);
+		map.put("dname","운영부");
+		map.put("loc","제주");
+		list.add(map);			
+		//요청이 유지도는 동안에는 이 주소번지로 공유가능함.
+		//공유가 안되면 NullPointerException -> 500 -> Runtime에러
+		req.setAttribute("list", list);
+		//jsp페이지 호출하기 -forward로 해야 한다.
+		//왜냐하면 servlet과 jsp가 요청이 계속 유지되고 있다 라고 생각
+		//비상태 프로토콜이란 요청 URL이 바뀌면 기존에 요청이 끊어지고 새로운요청이 발생함.
+		//유지가 안됨
+		RequestDispatcher view = req.getRequestDispatcher("/dept/deptList.jsp");
+		view.forward(req, resp);		
+		
+		
 	}//end of doGet
 	//입력, 저장, 파일업로드, 조회인데 보안상 값이 노출되지 않도록 할 때
 	/****************************************************************
@@ -42,10 +75,14 @@ public class CrudDeptServlet extends HttpServlet {
 	 *  @param 사용자가 입력한 부서명 - dname
 	 *  @param 사용자가 입력한 지역명 - loc
 	 *  @return int 1이면 입력 성공 0이면 입력 실패
+	 *  주의 : 브라우저의 URL을 통해서는 POST방식을 테스트할 수 없다.
+	 *  만일 요청을 해보면 조회가 된다. - 원하는게 아니다.-> 입력
+	 *  반드시 postman, swagger, javascript
 	 ****************************************************************/	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		log.info("doPost");
+		resp.sendRedirect("/dept/deptInsertOk.jsp");
 	}//end of doPost
 	//수정하기 구현
 	/****************************************************************
@@ -56,6 +93,13 @@ public class CrudDeptServlet extends HttpServlet {
 	 *  @param 사용자가 입력한 부서명 - dname
 	 *  @param 사용자가 입력한 지역명 - loc
 	 *  @param 사용자가 선택한 부서번호 - deptno
+	 *  @return int 1이면 수정 성공 0이면 수정 실패
+	 *  질문 : doPut메서드의 리턴 타입이 void인데 1 또는 0을 어떻게 jsp페이지에 
+	 *  전달할 수 있나요?
+	 *  1)query string을 이용한다.
+	 *  2)request scope를 이용한다.
+	 *  왜 jsp페이지에 1또는 0을 넘겨야 하나요?
+	 *  1이면 사용자에게 수정이 성공하였습니다. 또는 0일 땐 실패하였습니다. 후처리
 	 ****************************************************************/
 	@Override
 	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
--- src/main/webapp/dept/deptList.jsp
+++ src/main/webapp/dept/deptList.jsp
@@ -1,5 +1,11 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ page import="java.util.*" %>    
+<%
+	List<Map<String,Object>> list =
+		(List<Map<String,Object>>)request.getAttribute("list");
+	out.print(list);
+%>    
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,5 +14,23 @@
 </head>
 <body>
 	<h2>부서목록</h2>
+	<table border="1" width="500px" height="150px">
+		<tr>
+			<th>부서번호</th><th>부서명</th><th>지역</th>
+		</tr>
+<%
+//스크립틀릿 - 지역변수, 제어문, 연산자
+	for(int i=0;i<3;i++){
+		Map<String,Object> rmap = list.get(i);
+%>		
+		<tr>
+			<td><%=rmap.get("deptno") %></td>
+			<td><%=rmap.get("dname") %></td>
+			<td><%=rmap.get("loc") %></td>
+		</tr>
+<%
+	}
+%>		
+	</table>
 </body>
 </html>
